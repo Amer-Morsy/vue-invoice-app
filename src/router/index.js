dcb405e716f8ce49c054bd1fafef1802dd9f2857
Amer-Morsy <amermousa341@gmail.com>
2024-10-29 04:06:35 +0300

    clean

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import Invoice from "../views/InvoicePage.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/invoice/:invoiceId",
-    name: "Invoice",
-    component: Invoice,
   },
 ];
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,106 +1,11 @@
 <template>
-  <div class="home container">
-    <!-- Header -->
-    <div class="header flex">
-      <div class="left flex flex-column">
-        <h1>Invoices</h1>
-        <span>There are {{ invoiceData.length }} total invoices</span>
-      </div>
-      <div class="right flex">
-        <div class="filter flex">
-          <span>Filter By status</span>
-          <img src="@/assets/icon-arrow-down.svg" alt="" />
-        </div>
-        <div @click="newInvoice" class="button flex">
-          <div class="inner-button flex">
-            <img src="@/assets/icon-plus.svg" alt="" />
-          </div>
-          <span>New Invoice</span>
-        </div>
-      </div>
-    </div>
-    <!-- Invoices -->
-    <Invoice
-      v-for="(invoice, index) in invoiceData"
-      :key="index"
-      :invoice="invoice"
-    />
-  </div>
+  <div class="home container">home</div>
 </template>
 
 <script>
-import { mapMutations, mapState } from "vuex";
-import Invoice from "../components/InvoiceView.vue";
 export default {
   name: "HomeView",
-  components: {
-    Invoice,
-  },
-  methods: {
-    ...mapMutations({
-      newInvoice: "TOGGLE_INVOICE",
-    }),
-  },
-  computed: {
-    ...mapState({
-      invoiceData: "invoiceData",
-    }),
-  },
 };
 </script>
 
-<style lang="scss" scoped>
-.home {
-  color: #fff;
-
-  .header {
-    margin-bottom: 65px;
-    .left,
-    .right {
-      flex: 1;
-    }
-    .right {
-      justify-content: flex-end;
-      align-items: center;
-
-      .button,
-      .filter {
-        align-items: center;
-
-        span {
-          font-size: 12px;
-        }
-      }
-
-      .filter {
-        margin-right: 40px;
-        img {
-          margin-left: 12px;
-          width: 9px;
-          height: 5px;
-        }
-      }
-
-      .button {
-        cursor: pointer;
-        padding: 8px 10px;
-        background-color: #7c5dfa;
-        border-radius: 40px;
-
-        .inner-button {
-          margin-right: 8px;
-          border-radius: 50%;
-          padding: 8px;
-          align-items: center;
-          justify-content: center;
-          background-color: #fff;
-          img {
-            width: 10px;
-            height: 10px;
-          }
-        }
-      }
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
